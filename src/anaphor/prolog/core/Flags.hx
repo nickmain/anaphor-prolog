57b39017214a6701a43d62a7e771f9d8f4542da2
Nick Main <david.nick.main@gmail.com>
2020-10-15 20:47:45 -0700

    Create Flags.hx

---
+++ src/anaphor/prolog/core/Flags.hx
@@ -0,0 +1,23 @@
+// Copyright © 2020, David N Main. All rights reserved.
+// Licensed under the MIT License, see LICENSE file for details.
+
+package anaphor.prolog.core;
+using haxe.EnumTools;
+
+// ISO 7.11 Flags
+class Flags {
+
+    public static final flagNames = [
+        "bounded",
+        "max_integer",
+        "min_integer",
+        "integer_rounding_function",
+        "char_conversion",
+        "debug",
+        "max_arity",
+        "unknown",
+        "double_quotes"
+    ];
+    
+
+}
