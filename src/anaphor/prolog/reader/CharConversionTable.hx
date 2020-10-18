c5735741f0c1213103c7763caac8bf3eea26d706
Nick Main <david.nick.main@gmail.com>
2020-10-17 23:39:35 -0700

    Create CharConversionTable.hx

---
+++ src/anaphor/prolog/reader/CharConversionTable.hx
@@ -0,0 +1,10 @@
+// Copyright © 2020, David N Main. All rights reserved.
+// Licensed under the MIT License, see LICENSE file for details.
+
+package anaphor.prolog.reader;
+
+typedef CharConverter = (String) -> String;
+
+class CharConversionTable {
+    // TODO
+}
